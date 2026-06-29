3ccf92e0e28f0b2e92b8c9e4c6a4b9cf33209365
matheus <matheusaugustokirst@gmail.com>
2026-06-29 20:47:59 -0300

    Adiciona ImparOuPar

---
+++ Exercicios/ImparOuPar/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ImparOuPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+        public static void App()
+        {
+            while (true)
+            {
+                Console.WriteLine("--- Impar ou Par ---");
+                Console.WriteLine("Digite um número:");
+                string? inputNumero = Console.ReadLine();
+
+                if (inputNumero != null)
+                {
+                    try
+                    {
+                        int numero = int.Parse(inputNumero);
+                        if (numero % 2 == 0)
+                        {
+                            Console.WriteLine("O número é par.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("O número é ímpar.");
+                        }
+                    }
+                    catch {
+                        Console.WriteLine("Valor inválido.");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
--- Exercicios/OrientacaoAObjeto/Pedido.cs
+++ Exercicios/OrientacaoAObjeto/Pedido.cs
@@ -2,17 +2,17 @@
 {
     internal class Pedido
     {
-        public Pedido(Pessoa cliente, IList<Produto> produtos, DateTime dataDoServico)
+        public Pedido(Pessoa cliente, List<Produto> produtos, DateTime dataDoServico)
     {
             Cliente = cliente;
             DataDoServico = dataDoServico;
             Produtos = produtos;
             ValorTotal = CalcularValorTotal(produtos);
         }
-
+        
         public Pedido(Pessoa cliente,
             DateTime dataDoServico,
-            IList<Produto> produtos,
+            List<Produto> produtos,
             string observacao = "Nenhuma",
             bool isPago = false
             )
@@ -27,12 +27,12 @@
 
         public Pessoa Cliente { get; private set; }
         public DateTime DataDoServico { get; private set; }
-        public IList<Produto> Produtos { get; private set; }
+        public List<Produto> Produtos { get; private set; }
         public double? ValorTotal { get; private set; }
         public string? Observacao { get; set; }
         public bool IsPago { get; set; }
         
-        public static double CalcularValorTotal(IList<Produto> produtos)
+        public static double CalcularValorTotal(List<Produto> produtos)
         {
             double valorTotal = 0.00;
             foreach (var item in produtos)
